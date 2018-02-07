942f22d4a95d44c7a7a3b2fd468b36f5070c1f33
Ben Laurie <benl@google.com>
2018-02-07 10:58:15 +0000

    Start logging.

--- registers/TUTORIAL.md
+++ registers/TUTORIAL.md
@@ -14,3 +14,45 @@ You can run it with
 
 By default it uses the "register" register (a list of all registers)
 but you can point it at others with the ```--register``` flag.
+
+Step 2
+------
+
+Next, we are going to put the data into a Trillian log. The first step
+is run the log server and create a log.
+
+Build Trillian:
+
+```make trillian```
+
+Run the log server (this will tie up a terminal window):
+
+```make tlserver```
+
+And create a log (if you need to redo this, you'll need to delete
+logid):
+
+```make createlog```
+
+So now we have a log ready for entries from the register. To fill that
+log, we connect to the log using ```grpc.Dial()```, then create a new
+log client with ```trillian.NewTrillianLogClient```.
+
+We then feed each entry as we get it to the log, using
+```QueueLeaf()``` on the log client. The rest is just housekeeping.
+
+I chose to use JSON to encode the leaves because the registers
+themselves use JSON. JSON is not actually a very good encoding format,
+it is too limited, so I would normally advise something else, such as
+protobufs.
+
+You can run the logger with:
+
+```
+go run dump/main.go -log_id=`cat logid`
+```
+
+Note that if you run it a second time, it will not add any new entries
+(unless the underlying GDS register has changed) because duplicate
+entries are ignored by Trillian, by default at least. (FIXME: can
+dupes occur if added fast enough? Presumably yes?)
--- registers/dump/main.go
+++ registers/dump/main.go
@@ -1,21 +1,69 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/benlaurie/gds-registers/register"
+	"github.com/google/trillian"
+	"google.golang.org/genproto/googleapis/rpc/code"
+	"google.golang.org/grpc"
 )
 
 var (
-	regname = flag.String("register", "register", "name of register (e.g. 'country')")
+	regname     = flag.String("register", "register", "name of register (e.g. 'country')")
+	trillianLog = flag.String("trillian_log", "localhost:8090", "address of the Trillian Log RPC server.")
+	logId       = flag.Int64("log_id", 0, "Trillian LogID to populate.")
 )
 
 type dumper struct {
+	tc               trillian.TrillianLogClient
+	logId            int64
+	ctx              context.Context
+	newEntries       uint64
+	duplicateEntries uint64
 }
 
-func (*dumper) Process(e map[string]interface{}, h string, i map[string]interface{}) error {
+type leaf struct {
+	Entry map[string]interface{}
+	Hash  string
+	Item  map[string]interface{}
+}
+
+func (d *dumper) Process(e map[string]interface{}, h string, i map[string]interface{}) error {
 	log.Printf("%#v %s %#v", e, h, i)
+
+	// Put all three parts in a single structure and serialise to JSON
+	l := leaf{Entry: e, Hash: h, Item: i}
+	j, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+
+	// Send to Trillian
+	tl := &trillian.LogLeaf{LeafValue: j}
+	q := &trillian.QueueLeafRequest{LogId: d.logId, Leaf: tl}
+	r, err := d.tc.QueueLeaf(d.ctx, q)
+	if err != nil {
+		return err
+	}
+
+	// And check everything worked
+	c := code.Code(r.QueuedLeaf.GetStatus().GetCode())
+	// Do we really need to cast?
+	if c != code.Code_OK && c != code.Code_ALREADY_EXISTS {
+		return fmt.Errorf("Bad return status: %s", r.QueuedLeaf.Status)
+	}
+
+	// count
+	if c == code.Code_OK {
+		d.newEntries++
+	} else if c == code.Code_ALREADY_EXISTS {
+		d.duplicateEntries++
+	}
 	return nil
 }
 
@@ -28,8 +76,19 @@ func main() {
 	}
 	log.Printf("%#v", r)
 
-	err = r.GetEntries(&dumper{})
+	g, err := grpc.Dial(*trillianLog, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial Trillian Log: %v", err)
+	}
+
+	tc := trillian.NewTrillianLogClient(g)
+
+	d := &dumper{tc: tc, ctx: context.Background(), logId: *logId}
+	err = r.GetEntries(d)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Printf("New entries: %d", d.newEntries)
+	log.Printf("Duplicate entries: %d", d.duplicateEntries)
 }

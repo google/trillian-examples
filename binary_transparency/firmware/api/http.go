ea4bc99b7c2f777c277684897ded47de917a84a4
Martin Hutchinson <mhutchinson@google.com>
2020-11-05 15:45:10 +0000

    Implemented get-consistency to get proof of append-only log evolution
    Moved responses.go contents into http.go as these are only used in the http API at the moment. Creating a new file for requests.go would have been overkill and reduced readability. All of this can move around again if needed.
    
    Change-Id: I861b83fa0f9ea620cfd40d2d68b7ae469b4c8648

--- binary_transparency/firmware/api/http.go
+++ binary_transparency/firmware/api/http.go
@@ -1,5 +1,7 @@
 package api
 
+import "fmt"
+
 const (
 	// HTTPAddFirmware is the path of the URL to publish a firmware entry.
 	HTTPAddFirmware = "ft/v0/add-firmware"
@@ -10,3 +12,32 @@ const (
 	// HTTPGetRoot is the path of the URL to get a recent log root.
 	HTTPGetRoot = "ft/v0/get-root"
 )
+
+// LogCheckpoint commits to the state of the log.
+// TODO(mhutchinson): This needs a signature to be worth anything, which
+// requires a known serialization. This will be changed in the future but works
+// well enough for the state of the demo at this time.
+type LogCheckpoint struct {
+	TreeSize uint64
+	RootHash []byte
+	// The number of nanoseconds since the Unix epoch.
+	TimestampNanos uint64
+}
+
+// String returns a compact printable representation of a LogCheckpoint.
+func (l LogCheckpoint) String() string {
+	return fmt.Sprintf("{size %d @ %d root: 0x%x}", l.TreeSize, l.TimestampNanos, l.RootHash)
+}
+
+// GetConsistencyRequest is sent to ask for a proof that the tree at ToSize
+// is append-only from the tree at FromSize. The response is a ConsistencyProof.
+type GetConsistencyRequest struct {
+	FromSize uint64
+	ToSize   uint64
+}
+
+// ConsistencyProof contains the hashes to demonstrate the append-only evolution
+// of the log.
+type ConsistencyProof struct {
+	Proof [][]byte
+}

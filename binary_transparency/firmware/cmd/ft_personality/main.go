e53e0eb6ffc9f0751b43c42198af030b22216488
Martin Hutchinson <mhutchinson@gmail.com>
2020-11-05 13:30:36 +0000

    Talk to Trillian directly instead of via the client
    This will allow us to return consistency and inclusion proofs. It also allows how STHs are refreshed to be more explicitly visible. This change adds in a periodic task to fetch the latest from Trillian.
    
    Change-Id: Iad115f62c344231451c4d6e74382fce095baae82

--- binary_transparency/firmware/cmd/ft_personality/main.go
+++ binary_transparency/firmware/cmd/ft_personality/main.go
@@ -24,20 +24,38 @@ var (
 	connectTimeout = flag.Duration("connect_timeout", time.Second, "the timeout for connecting to the backend")
 	trillianAddr   = flag.String("trillian", ":8090", "address:port of Trillian Log gRPC service")
 	treeID         = flag.Int64("tree_id", -1, "the tree ID of the log to use")
+
+	sthRefresh = flag.Duration("sth_refresh_interval", 5*time.Second, "how often to fetch the latest log root from Trillian")
 )
 
 func main() {
 	flag.Parse()
 
+	ctx := context.Background()
+
 	if *treeID < 0 {
 		glog.Exitf("tree_id is required")
 	}
 	glog.Infof("Connecting to Trillian Log...")
-	tclient, err := trillian.NewClient(context.Background(), *connectTimeout, *trillianAddr, *treeID)
+	tclient, err := trillian.NewClient(ctx, *connectTimeout, *trillianAddr, *treeID)
 	if err != nil {
 		glog.Exitf("failed to connect to Trillian: %v", err)
 	}
 	defer tclient.Close()
+
+	// Periodically sync the golden STH in the background.
+	go func() {
+		for ctx.Err() == nil {
+			if err := tclient.UpdateRoot(ctx); err != nil {
+				glog.Warningf("error updating STH: %v", err)
+			}
+
+			select {
+			case <-ctx.Done():
+			case <-time.After(*sthRefresh):
+			}
+		}
+	}()
 
 	glog.Infof("Starting FT personality server...")
 	srv := ih.NewServer(tclient)

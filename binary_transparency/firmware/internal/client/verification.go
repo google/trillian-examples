180613f0f205c0ac962013dc711be986d83b8169
Al Cutter <al@google.com>
2020-11-10 14:46:24 +0000

    Add support for inclusion proofs in client
    Change-Id: Ifc85e3ac01503ccd83291fb72d80102ec64f6f3a

---
+++ binary_transparency/firmware/internal/client/verification.go
@@ -0,0 +1,17 @@
+package client
+
+import (
+	"github.com/google/trillian/merkle"
+	"github.com/google/trillian/merkle/rfc6962"
+)
+
+// NewLogVerifier is a convenience function to return a log verifier suitable for use
+// with the FT personality.
+func NewLogVerifier() merkle.LogVerifier {
+	return merkle.NewLogVerifier(rfc6962.DefaultHasher)
+}
+
+// HashLeaf is a convenience function to return a leaf hash for a given leaf.
+func HashLeaf(leaf []byte) []byte {
+	return rfc6962.DefaultHasher.HashLeaf(leaf)
+}

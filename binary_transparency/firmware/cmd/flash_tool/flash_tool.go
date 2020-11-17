e056a900eb3bd8987d74652dda29b12db98331f3
Al Cutter <al@google.com>
2020-11-17 19:50:46 +0000

    Add license headers

--- binary_transparency/firmware/cmd/flash_tool/flash_tool.go
+++ binary_transparency/firmware/cmd/flash_tool/flash_tool.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // flash_tool is a util to flash firmware update packages created by the publisher tool onto devices.
 //
 // Currently, the only device is a dummy device, which simply sorts the firmware+metadata on local disk.
--- binary_transparency/firmware/cmd/ft_personality/main.go
+++ binary_transparency/firmware/cmd/ft_personality/main.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // This package is the entrypoint for the Firmware Transparency personality server.
 // This requires a Trillian instance to be reachable via gRPC and a tree to have
 // been provisioned. See the README in the root of this project for instructions.
@@ -12,9 +26,9 @@ import (
 	"time"
 
 	"github.com/golang/glog"
-	"github.com/gorilla/mux"
 	ih "github.com/google/trillian-examples/binary_transparency/firmware/cmd/ft_personality/internal/http"
 	"github.com/google/trillian-examples/binary_transparency/firmware/cmd/ft_personality/internal/trillian"
+	"github.com/gorilla/mux"
 
 	_ "github.com/google/trillian/merkle/rfc6962" // Load hashers
 )

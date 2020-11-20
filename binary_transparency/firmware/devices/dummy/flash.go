f2e076ec6fc97aa2f9b8e95f52abd987900f9b17
Al Cutter <al@google.com>
2020-11-20 09:47:11 +0000

    Factor out devices bits from cmds (#144)

--- binary_transparency/firmware/devices/dummy/flash.go
+++ binary_transparency/firmware/devices/dummy/flash.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package devices
+package dummy
 
 import (
 	"encoding/json"
@@ -23,6 +23,7 @@ import (
 	"path/filepath"
 
 	"github.com/google/trillian-examples/binary_transparency/firmware/api"
+	"github.com/google/trillian-examples/binary_transparency/firmware/cmd/flash_tool/devices"
 )
 
 var (
@@ -34,17 +35,17 @@ const (
 	firmwarePath = "firmware.bin"
 )
 
-// DummyDevice is a fake device using the local filesystem for storage.
-type DummyDevice struct {
+// Device is a fake device using the local filesystem for storage.
+type Device struct {
 	// bundle holds all the update data except the firmware image.
 	bundle api.ProofBundle
 }
 
-var _ Device = DummyDevice{}
+var _ devices.Device = Device{}
 
-// NewDummyDeviceFromFlags creates a new dummy device instance using data from flags.
+// NewFromFlags creates a new dummy device instance using data from flags.
 // TODO(al): figure out how/whether to remove the flag from in here.
-func NewDummyDeviceFromFlags() (*DummyDevice, error) {
+func NewFromFlags() (*Device, error) {
 	dStat, err := os.Stat(*dummyDirectory)
 	if err != nil {
 		return nil, fmt.Errorf("unable to stat dummy_storage_dir %q: %w", *dummyDirectory, err)
@@ -53,13 +54,13 @@ func NewDummyDeviceFromFlags() (*DummyDevice, error) {
 		return nil, fmt.Errorf("dummy_storage_dir %q is not a directory", *dummyDirectory)
 	}
 
-	d := &DummyDevice{}
+	d := &Device{}
 
 	fPath := filepath.Join(*dummyDirectory, bundlePath)
 	f, err := os.OpenFile(fPath, os.O_RDONLY, os.ModePerm)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return d, ErrNeedsInit(fmt.Errorf("couldn't read bundle file %q: %w", fPath, err))
+			return d, devices.ErrNeedsInit(fmt.Errorf("couldn't read bundle file %q: %w", fPath, err))
 		}
 		return d, fmt.Errorf("failed to read bundle file %q: %w", fPath, err)
 	}
@@ -70,14 +71,14 @@ func NewDummyDeviceFromFlags() (*DummyDevice, error) {
 }
 
 // DeviceCheckpoint returns the latest log checkpoint stored on the device.
-func (d DummyDevice) DeviceCheckpoint() (api.LogCheckpoint, error) {
+func (d Device) DeviceCheckpoint() (api.LogCheckpoint, error) {
 	return d.bundle.Checkpoint, nil
 }
 
 // ApplyUpdate applies the firmware update to the dummy device.
 // The firmware image is stored in the dummy state directory in the firmware.bin file,
 // and the rest of the update bundle is stored in the bundle.json file.
-func (d DummyDevice) ApplyUpdate(u api.UpdatePackage) error {
+func (d Device) ApplyUpdate(u api.UpdatePackage) error {
 	fwFile := filepath.Join(*dummyDirectory, firmwarePath)
 	bundleFile := filepath.Join(*dummyDirectory, bundlePath)
 

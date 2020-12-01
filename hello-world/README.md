17e439469d3fcffea768787c2a89af2ad162124a
Sarah Meiklejohn <meiklejohn@google.com>
2020-12-01 10:36:43 +0000

    hello world example

---
+++ hello-world/README.md
@@ -0,0 +1,38 @@
+# hello-world
+This is a very basic example of how to use
+[Trillian](http://github.com/google/trillian) to implement the operations
+needed for a verifiable log.  It intentionally avoids
+defining any interface between the client and personality, leaving only the
+gRPC interface between the personality and the (Trillian) log.  In a real
+deployment it would be necessary to define an interface between the client
+and the personality, and thus implement a "real" client on top of the
+current one.
+
+In order to run the tests, it is necessary to first have a Trillian log
+running, which itself means it is necessary to have MySQL or MariaDB
+running (see [MySQL
+setup](http://github.com/google/trillian#mysqlsetup) for more
+information).  In one terminal, run the following commands:
+
+```
+export GO111MODULE=on
+go run github.com/google/trillian/cmd/trillian_log_server --rpc_endpoint="localhost:50054" --http_endpoint="localhost:50055" &
+go run github.com/google/trillian/cmd/trillian_log_signer --sequencer_interval="1s" --batch_size=500 --rpc_endpoint="localhost:50056" --http_endpoint="localhost:50057" --num_sequencers=1 --force_master &
+# Trillian services are now running
+go run github.com/google/trillian/cmd/createtree --admin_server=localhost:50054 # Take note of this tree/log ID
+
+export TREE_ID= # Use the tree ID returned by createtree above
+go run github.com/google/trillian/cmd/get_tree_public_key --admin_server=localhost:50054 --log_id=${TREE_ID}
+```
+
+Paste the public key returned by this last call into `config.go` and paste
+the tree ID into `personality.go`.
+
+Now, in another terminal run `go build` and then `go test`.  This runs
+three tests. 
+1. `TestAppend` ensures that checkpoints update properly on the
+personality side when new entries are appended to the log.
+2. `TestUpdate` ensures that these updated checkpoints can be accepted by the 
+client, after they are proved consistent with previous checkpoints.
+3. `TestIncl` ensures that the client can be convinced about whether or not 
+entries are in the log.
---
+++ hello-world/helloworld_test.go
@@ -0,0 +1,128 @@
+// Copyright 2020 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helloworld
+
+import (
+    "context"
+    "math/rand"
+    "fmt"
+    "testing"
+    "time"
+
+    p "github.com/google/trillian-examples/hello-world/personality"
+)
+
+// TestAppend appends a random entry to the log and ensures that the
+// checkpoint updates properly (locally on the personality's side).
+func TestAppend(t *testing.T) {
+    name := "testAppend"
+    t.Run(name, func(t *testing.T) {
+	ctx := context.Background()
+	personality := p.NewPersonality()
+	chkptOld := personality.GetChkpt(ctx)
+	// Add a random entry so we can be sure it's new.
+	entry := make([]byte, 10)
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(entry)
+	chkptNew := personality.Append(ctx, entry)
+	if chkptNew.LogSize <= chkptOld.LogSize {
+	    t.Errorf("the log didn't grow properly in %v", name)
+	}
+	fmt.Printf("success in %v, new log size is %v\n", name, chkptNew.LogSize)
+    })
+}
+
+// TestUpdate appends a random entry to the log and ensures that the
+// checkpoint updates properly for both the personality and the verifier.
+func TestUpdate(t *testing.T) {
+    name := "testUpdate"
+    t.Run(name, func(t *testing.T) {
+	ctx := context.Background()
+	personality := p.NewPersonality()
+	client := NewClient(personality)
+	chkpt := personality.GetChkpt(ctx)
+	client.chkpt = chkpt
+	entry := make([]byte, 10)
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(entry)
+	personality.Append(ctx, entry)
+	chkptNew, pf := personality.UpdateChkpt(ctx, chkpt)
+	got := client.UpdateChkpt(chkptNew, pf)
+	if !got {
+	    t.Errorf("verifier failed to update checkpoint")
+	}
+	fmt.Printf("success in %v, new log size is %v\n", name, chkpt.LogSize)
+    })
+}
+
+// TestIncl tests inclusion proof checking for entries that both are and
+// aren't in the log.
+func TestIncl(t *testing.T) {
+    tests := []struct {
+	name           string
+	addEntries     []string
+	checkEntries   []string
+	wants          []bool
+    }{
+	{
+	    name:         "all there",
+	    addEntries:   []string{"a", "b", "c", "d"},
+	    checkEntries: []string{"a", "b", "c", "d"},
+	    wants:        []bool{true, true, true, true},
+	},
+	{
+	    name:         "all missing",
+	    addEntries:   []string{"a", "b", "c", "d"},
+	    checkEntries: []string{"w", "x", "y", "z"},
+	    wants:        []bool{false, false, false, false},
+	},
+	{
+	    name:         "mixed bag",
+	    addEntries:   []string{"a", "b", "c", "d"},
+	    checkEntries: []string{"a", "b", "y", "z"},
+	    wants:        []bool{true, true, false, false},
+	},
+    }
+
+    for _, test := range tests {
+	test := test
+	t.Run(test.name, func(t *testing.T) {
+	    ctx := context.Background()
+	    personality := p.NewPersonality()
+	    var chkpt p.Chkpt
+            // Append all the entries we plan to add (some of them might be
+            // there already).
+	    for _, entry := range test.addEntries {
+		bs := []byte(entry)
+		chkpt = personality.Append(ctx, bs)
+	    }
+	    client := NewClient(personality)
+	    // For the purposes of the test let's skip having the verifier 
+	    // update the right way and just assign their checkpoint.
+	    client.chkpt = chkpt
+	    // Then prove and check inclusion of the other entries.
+	    for i, _ := range test.checkEntries {
+		entry := test.checkEntries[i]
+		bs := []byte(entry)
+		pf := personality.ProveIncl(ctx, chkpt, bs)
+		got := client.VerIncl(bs, pf)
+		if got != test.wants[i] {
+		    t.Errorf("%v: got %v, want %v", test.name, got, test.wants[i])
+		}
+	    }
+	    fmt.Printf("testIncl: all good for the %v test!\n", test.name)
+	})
+    }
+}

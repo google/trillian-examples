0ad3c55e6f11eba9360f19f8afea68760ced7e1d
Martin Hutchinson <mhutchinson@gmail.com>
2020-12-10 16:52:16 +0000

    Added a simple witness to the SumDB examples
    This allows the mirror to be easily upgraded to provide a service which is easy to integrate with from any other tooling.

--- sumdbaudit/audit/service.go
+++ sumdbaudit/audit/service.go
@@ -34,6 +34,26 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+// CheckpointTooFresh is returned when a consistency proof is requested for a
+// checkpoint that is larger than the currently synced size.
+type CheckpointTooFresh struct {
+	RequestedSize, MaxSize int64
+}
+
+func (e CheckpointTooFresh) Error() string {
+	return fmt.Sprintf("requested checkpoint is at size %d but local log only has %d entries", e.RequestedSize, e.MaxSize)
+}
+
+// InconsistentCheckpoints is returned when a consistency proof cannot be found
+// between two checkpoints.
+type InconsistentCheckpoints struct {
+	A, B *Checkpoint
+}
+
+func (e InconsistentCheckpoints) Error() string {
+	return fmt.Sprintf("checkpoints at size %d (%x) and size %d (%x) are inconsistent", e.A.N, e.A.Hash, e.B.N, e.B.Hash)
+}
+
 // Service has all the operations required for an auditor to verifiably clone
 // the remote SumDB.
 type Service struct {
@@ -134,6 +154,52 @@ func (s *Service) ProcessMetadata(ctx context.Context, checkpoint *Checkpoint) e
 		}
 	}
 	return nil
+}
+
+// CheckConsistency checks that the given checkpoint is consistent with the state
+// downloaded and committed to by the current Golden Checkpoint. If the given
+// checkpoint is too recent then a CheckpointTooFresh error will be returned. If
+// the checkpoint is inconsistent with the golden checkpoint then the error will
+// be of type InconsistentCheckpoints.
+func (s *Service) CheckConsistency(ctx context.Context, checkpoint *Checkpoint) error {
+	golden, err := s.localDB.GoldenCheckpoint(s.ParseCheckpointNote)
+	if err != nil {
+		return fmt.Errorf("failed to query for golden checkpoint: %v", err)
+	}
+	if bytes.Equal(checkpoint.Raw, golden.Raw) {
+		// This is easy and possibly common, so handle it as a special case.
+		return nil
+	}
+	head, err := s.localDB.Head()
+	if err != nil {
+		return fmt.Errorf("cannot find head of local log: %v", err)
+	}
+	if checkpoint.N > head {
+		// If we wanted to, we _could_ check consistency for checkpoints that
+		// are larger than this, but it would require querying the log. For the
+		// sake of a tight design, we will only try to perform checks for data
+		// which we have entirely locally.
+		return CheckpointTooFresh{
+			RequestedSize: checkpoint.N,
+			MaxSize:       head}
+	}
+
+	err = s.checkCheckpoint(checkpoint, func(stragglerCount int) ([][]byte, error) {
+		stragglerTileOffset := int(checkpoint.N / (1 << s.height))
+		return s.localDB.Tile(s.height, 0, stragglerTileOffset)
+	})
+	if err != nil {
+		return InconsistentCheckpoints{
+			A: golden,
+			B: checkpoint,
+		}
+	}
+	return nil
+}
+
+// ParseCheckpointNote parses the raw checkpoint.
+func (s *Service) ParseCheckpointNote(raw []byte) (*Checkpoint, error) {
+	return s.sumDB.ParseCheckpointNote(raw)
 }
 
 // VerifyTiles checks that every tile calculated locally matches the result returned

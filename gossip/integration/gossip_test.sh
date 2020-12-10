0d195b087f49a9c2f75ba279dfcdbc52b59fa5c7
Al Cutter <al@google.com>
2020-12-10 14:26:38 +0000

    Assume an ambient Trillian

--- gossip/integration/gossip_test.sh
+++ gossip/integration/gossip_test.sh
@@ -4,29 +4,25 @@ set -e
 INTEGRATION_DIR="$( cd "$( dirname "$0" )" && pwd )"
 . "${INTEGRATION_DIR}"/gossip_functions.sh
 
-# Default to one of everything.
-LOG_SERVER_COUNT=${1:-1}
-LOG_SIGNER_COUNT=${2:-1}
-HUB_SERVER_COUNT=${3:-1}
+# Trillian must already be running
+[ -z ${TRILLIAN_LOG_RPC+x} ] && TRILLIAN_LOG_RPC="localhost:8090"
 
-gossip_prep_test "${LOG_SERVER_COUNT}" "${LOG_SIGNER_COUNT}" "${HUB_SERVER_COUNT}"
+gossip_prep_test 1
 
 # Cleanup for the Trillian components
 TO_DELETE="${TO_DELETE} ${ETCD_DB_DIR} ${PROMETHEUS_CFGDIR}"
-TO_KILL+=(${LOG_SIGNER_PIDS[@]})
-TO_KILL+=(${RPC_SERVER_PIDS[@]})
 TO_KILL+=(${ETCD_PID})
 TO_KILL+=(${PROMETHEUS_PID})
 TO_KILL+=(${ETCDISCOVER_PID})
 
 # Cleanup for the personality
 TO_DELETE="${TO_DELETE} ${HUB_CFG} ${SRC_PRIV_KEYS}"
-TO_KILL+=(${HUB_SERVER_PIDS[@]})
+TO_KILL+=(${HUB_SERVER_PID})
 
 echo "Running test(s)"
 pushd "${INTEGRATION_DIR}"
 set +e
-go test -v -run ".*LiveGossip.*" --timeout=5m ./ --hub_servers=${HUB_SERVERS} --hub_config=${HUB_CFG} --src_priv_keys=${SRC_PRIV_KEY_LIST}
+go test -v -run ".*LiveGossip.*" --timeout=5m ./ --hub_servers=${HUB_SERVER} --hub_config=${HUB_CFG} --src_priv_keys=${SRC_PRIV_KEY_LIST}
 RESULT=$?
 set -e
 popd

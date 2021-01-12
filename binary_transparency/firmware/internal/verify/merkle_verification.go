2c1f2c25eac41ca1429e26a0ce66f5ebe83994f1
Al Cutter <al@google.com>
2021-01-12 14:47:20 +0000

    FT: Use Trillian verifier/hasher from new location

--- binary_transparency/firmware/internal/verify/merkle_verification.go
+++ binary_transparency/firmware/internal/verify/merkle_verification.go
@@ -15,17 +15,17 @@
 package verify
 
 import (
-	"github.com/google/trillian/merkle"
-	"github.com/google/trillian/merkle/rfc6962"
+	"github.com/google/trillian/merkle/logverifier"
+	"github.com/google/trillian/merkle/rfc6962/hasher"
 )
 
 // NewLogVerifier is a convenience function to return a log verifier suitable for use
 // with the FT personality.
-func NewLogVerifier() merkle.LogVerifier {
-	return merkle.NewLogVerifier(rfc6962.DefaultHasher)
+func NewLogVerifier() logverifier.LogVerifier {
+	return logverifier.New(hasher.DefaultHasher)
 }
 
 // HashLeaf is a convenience function to return a leaf hash for a given leaf.
 func HashLeaf(leaf []byte) []byte {
-	return rfc6962.DefaultHasher.HashLeaf(leaf)
+	return hasher.DefaultHasher.HashLeaf(leaf)
 }

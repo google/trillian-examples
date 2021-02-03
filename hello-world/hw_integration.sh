586a5e73c63146ccc6fb7478ff8e4ded0ef4cb92
Sarah Meiklejohn <meiklejohn@google.com>
2021-02-03 15:59:08 +0000

    added missing flag for the cloud build

--- hello-world/hw_integration.sh
+++ hello-world/hw_integration.sh
@@ -2,5 +2,5 @@
 
 export TREE_ID=$(go run github.com/google/trillian/cmd/createtree --admin_server=${TRILLIAN_LOG_RPC})
 
-go test ./hello-world/. --trillian=${TRILLIAN_LOG_RPC} --tree_id=${TREE_ID}
+go test ./hello-world/. --trillian=${TRILLIAN_LOG_RPC} --tree_id=${TREE_ID} --log_addr=${TRILLIAN_LOG_RPC}
 

3f0ca44294c212817b5e16f7985a0013a5e12cc0
Sarah Meiklejohn <meiklejohn@google.com>
2021-02-04 15:26:46 +0000

    gofmt to make the linter happy

---
+++ helloworld/client.go
@@ -0,0 +1,90 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Runs a simple client, designed to interact with a personality.
+package helloworld
+
+import (
+	"context"
+
+	trillian "github.com/google/trillian"
+	"github.com/google/trillian/merkle"
+	"github.com/google/trillian/merkle/rfc6962"
+
+	p "github.com/google/trillian-examples/helloworld/personality"
+)
+
+// Personality is the interface to the the Trillian log.
+type Personality interface {
+	// Append stores an entry in the log.
+	Append(context.Context, []byte) p.Chkpt
+
+	// ProveIncl proves the inclusion of a given entry.
+	ProveIncl(context.Context, p.Chkpt, []byte) *trillian.Proof
+
+	// UpdateChkpt provides a new checkpoint and proves it is consistent
+	// with an old one.
+	UpdateChkpt(context.Context, p.Chkpt) (p.Chkpt, *trillian.Proof)
+}
+
+// A client is a verifier that maintains a checkpoint as state.
+type Client struct {
+	v      merkle.LogVerifier
+	chkpt  p.Chkpt
+	person Personality
+}
+
+// NewClient creates a new client with an empty checkpoint and a given
+// personality to talk to.
+func NewClient(prsn Personality) Client {
+	c := Client{person: prsn}
+	v := merkle.NewLogVerifier(rfc6962.DefaultHasher)
+	c.v = v
+	var rootHash []byte
+	chkpt := p.Chkpt{LogSize: 0, RootHash: rootHash}
+	c.chkpt = chkpt
+	return c
+}
+
+// VerIncl allows the client to check inclusion of a given entry.
+func (c Client) VerIncl(entry []byte, pf *trillian.Proof) bool {
+	if pf == nil {
+		return false
+	}
+	leafHash := rfc6962.DefaultHasher.HashLeaf(entry)
+	if err := c.v.VerifyInclusionProof(pf.LeafIndex, c.chkpt.LogSize,
+		pf.Hashes, c.chkpt.RootHash, leafHash); err != nil {
+		return false
+	}
+	return true
+}
+
+// UpdateChkpt allows a client to update its stored checkpoint.
+func (c Client) UpdateChkpt(chkptNew p.Chkpt, pf *trillian.Proof) bool {
+	// If there is no checkpoint then just use this one no matter what.
+	if c.chkpt.LogSize != 0 {
+		// Else make sure this new checkpoint is consistent with the current one.
+		if pf == nil {
+			return false
+		}
+		hashes := pf.GetHashes()
+		if err := c.v.VerifyConsistencyProof(c.chkpt.LogSize, chkptNew.LogSize,
+			c.chkpt.RootHash, chkptNew.RootHash, hashes); err != nil {
+			return false
+		}
+	}
+	// If all is good then set this as the new checkpoint.
+	c.chkpt = chkptNew
+	return true
+}
---
+++ helloworld/helloworld_test.go
@@ -0,0 +1,146 @@
+// Copyright 2020 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helloworld
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	p "github.com/google/trillian-examples/helloworld/personality"
+)
+
+var (
+	trillianAddr = flag.String("trillian", "localhost:50054", "Host:port of Trillian Log RPC server")
+	treeID       = flag.Int64("tree_id", 0, "Tree ID")
+)
+
+// TestAppend appends a random entry to the log and ensures that the
+// checkpoint updates properly (locally on the personality's side).
+func TestAppend(t *testing.T) {
+	if *treeID == 0 {
+		t.Skip("--treeID flag unset, skipping test")
+	}
+
+	name := "testAppend"
+	t.Run(name, func(t *testing.T) {
+		ctx := context.Background()
+		personality := p.NewPersonality(*trillianAddr, *treeID)
+		chkptOld := personality.GetChkpt(ctx)
+		// Add a random entry so we can be sure it's new.
+		entry := make([]byte, 10)
+		rand.Seed(time.Now().UnixNano())
+		rand.Read(entry)
+		chkptNew := personality.Append(ctx, entry)
+		if chkptNew.LogSize <= chkptOld.LogSize {
+			t.Errorf("the log didn't grow properly in %v", name)
+		}
+		fmt.Printf("success in %v, new log size is %v\n", name, chkptNew.LogSize)
+	})
+}
+
+// TestUpdate appends a random entry to the log and ensures that the
+// checkpoint updates properly for both the personality and the verifier.
+func TestUpdate(t *testing.T) {
+	if *treeID == 0 {
+		t.Skip("--treeID flag unset, skipping test")
+	}
+
+	name := "testUpdate"
+	t.Run(name, func(t *testing.T) {
+		ctx := context.Background()
+		personality := p.NewPersonality(*trillianAddr, *treeID)
+		client := NewClient(personality)
+		chkpt := personality.GetChkpt(ctx)
+		client.chkpt = chkpt
+		entry := make([]byte, 10)
+		rand.Seed(time.Now().UnixNano())
+		rand.Read(entry)
+		personality.Append(ctx, entry)
+		chkptNew, pf := personality.UpdateChkpt(ctx, chkpt)
+		got := client.UpdateChkpt(chkptNew, pf)
+		if !got {
+			t.Errorf("verifier failed to update checkpoint")
+		}
+		fmt.Printf("success in %v, new log size is %v\n", name, chkpt.LogSize)
+	})
+}
+
+// TestIncl tests inclusion proof checking for entries that both are and
+// aren't in the log.
+func TestIncl(t *testing.T) {
+	if *treeID == 0 {
+		t.Skip("--treeID flag unset, skipping test")
+	}
+
+	tests := []struct {
+		name         string
+		addEntries   []string
+		checkEntries []string
+		wants        []bool
+	}{
+		{
+			name:         "all there",
+			addEntries:   []string{"a", "b", "c", "d"},
+			checkEntries: []string{"a", "b", "c", "d"},
+			wants:        []bool{true, true, true, true},
+		},
+		{
+			name:         "all missing",
+			addEntries:   []string{"a", "b", "c", "d"},
+			checkEntries: []string{"w", "x", "y", "z"},
+			wants:        []bool{false, false, false, false},
+		},
+		{
+			name:         "mixed bag",
+			addEntries:   []string{"a", "b", "c", "d"},
+			checkEntries: []string{"a", "b", "y", "z"},
+			wants:        []bool{true, true, false, false},
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			ctx := context.Background()
+			personality := p.NewPersonality(*trillianAddr, *treeID)
+			var chkpt p.Chkpt
+			// Append all the entries we plan to add (some of them might be
+			// there already).
+			for _, entry := range test.addEntries {
+				bs := []byte(entry)
+				chkpt = personality.Append(ctx, bs)
+			}
+			client := NewClient(personality)
+			// For the purposes of the test let's skip having the verifier
+			// update the right way and just assign their checkpoint.
+			client.chkpt = chkpt
+			// Then prove and check inclusion of the other entries.
+			for i := range test.checkEntries {
+				entry := test.checkEntries[i]
+				bs := []byte(entry)
+				pf := personality.ProveIncl(ctx, chkpt, bs)
+				got := client.VerIncl(bs, pf)
+				if got != test.wants[i] {
+					t.Errorf("%v: got %v, want %v", test.name, got, test.wants[i])
+				}
+			}
+			fmt.Printf("testIncl: all good for the %v test!\n", test.name)
+		})
+	}
+}

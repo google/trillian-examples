5f40adb7fe483965e0e01d3a2fa512d33448221a
Yogesh Deshpande <yogesh.deshpande@arm.com>
2021-02-12 07:09:41 -0500

    First revision of FT Witness Client side changes

---
+++ binary_transparency/firmware/internal/client/wclient.go
@@ -0,0 +1,65 @@
+// Copyright 2021 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/google/trillian-examples/binary_transparency/firmware/api"
+	"google.golang.org/grpc/status"
+)
+
+// WitnessClient is an HTTP client for the FT witness.
+type WitnessClient struct {
+	// LogURL is the base URL for the FT witness.
+	LogURL *url.URL
+}
+
+// GetWitnessCheckpoint returns a checkpoint from witness server
+func (c WitnessClient) GetWitnessCheckpoint() (*api.LogCheckpoint, error) {
+	u, err := c.LogURL.Parse(api.WitnessGetCheckpoint)
+	if err != nil {
+		return nil, err
+	}
+	r, err := http.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+	if r.StatusCode != 200 {
+		return &api.LogCheckpoint{}, errFromRsp("failed to fetch checkpoint", r)
+	}
+
+	var wcp api.LogCheckpoint
+	if err := json.NewDecoder(r.Body).Decode(&wcp); err != nil {
+		return nil, err
+	}
+	// TODO(yd): Check signature, when it is added
+	return &wcp, nil
+}
+
+func errFromRsp(m string, r *http.Response) error {
+	if r.StatusCode == 200 {
+		return nil
+	}
+
+	b, _ := ioutil.ReadAll(r.Body) // Ignore any error, we want to ensure we return the right status code which we already know.
+
+	msg := fmt.Sprintf("%s: %s", m, string(b))
+	return status.New(codeFromHTTPResponse(r.StatusCode), msg).Err()
+}
---
+++ binary_transparency/firmware/internal/client/wclient_test.go
@@ -0,0 +1,79 @@
+// Copyright 2021 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/trillian-examples/binary_transparency/firmware/api"
+)
+
+func TestGetWitnessCheckpoint(t *testing.T) {
+	for _, test := range []struct {
+		desc    string
+		body    string
+		want    api.LogCheckpoint
+		wantErr bool
+	}{
+		{
+			desc: "valid 1",
+			body: `{ "TreeSize": 1, "TimestampNanos": 123, "RootHash": "EjQ="}`,
+			want: api.LogCheckpoint{TreeSize: 1, TimestampNanos: 123, RootHash: []byte{0x12, 0x34}},
+		}, {
+			desc: "valid 2",
+			body: `{ "TreeSize": 10, "TimestampNanos": 1230, "RootHash": "NBI="}`,
+			want: api.LogCheckpoint{TreeSize: 10, TimestampNanos: 1230, RootHash: []byte{0x34, 0x12}},
+		}, {
+			desc:    "garbage",
+			body:    `garbage`,
+			wantErr: true,
+		},
+	} {
+		t.Run(test.desc, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if !strings.HasSuffix(r.URL.Path, api.WitnessGetCheckpoint) {
+					t.Fatalf("Got unexpected HTTP request on %q", r.URL.Path)
+				}
+				fmt.Fprintln(w, test.body)
+			}))
+			defer ts.Close()
+
+			tsURL, err := url.Parse((ts.URL))
+			if err != nil {
+				t.Fatalf("Failed to parse test server URL: %v", err)
+			}
+			wc := WitnessClient{LogURL: tsURL}
+			cp, err := wc.GetWitnessCheckpoint()
+			switch {
+			case err != nil && !test.wantErr:
+				t.Fatalf("Got unexpected error %q", err)
+			case err == nil && test.wantErr:
+				t.Fatal("Got no error, but wanted error")
+			case err != nil && test.wantErr:
+				// expected error
+			default:
+				if d := cmp.Diff(*cp, test.want); len(d) != 0 {
+					t.Fatalf("Got checkpoint with diff: %s", d)
+				}
+			}
+		})
+	}
+}

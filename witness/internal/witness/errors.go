5dcf7b7871b08572afc6271e947d0c67362c8d9b
Sarah Meiklejohn <meiklejohn@google.com>
2021-05-19 13:48:34 +0100

    first (tiny) commit for witness code

---
+++ witness/internal/witness/errors.go
@@ -0,0 +1,37 @@
+
+package witness
+
+import (
+	"fmt"
+
+	"github.com/google/trillian-examples/formats/log"
+)
+
+// ErrInconsistency should be returned when there has been an error proving
+// consistency between two checkpoints.
+type ErrInconsistency struct {
+	Smaller log.Checkpoint
+	Larger  log.Checkpoint
+	Proof   log.Proof
+
+	Wrapped error
+}
+
+func (e ErrInconsistency) Unwrap() error {
+	return e.Wrapped
+}
+
+func (e ErrInconsistency) Error() string {
+	return fmt.Sprintf("log consistency check failed: %s", e.Wrapped)
+}
+
+// ErrStale should be returned when Update has been called with `from` set to
+// something other than the latest checkpoint.
+type ErrStale struct {
+	Smaller log.Checkpoint
+	Latest  *log.Checkpoint
+}
+
+func (e ErrStale) Error() string {
+	return fmt.Sprintf("stale checkpoint given as input")
+}

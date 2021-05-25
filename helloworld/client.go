74d2cb09284fdab1649dad8147bea56473cbfef4
Al Cutter <al-github@9600.org>
2021-05-25 16:24:43 +0100

    [Helloworld] Add signature checking to helloworld (#340)

--- helloworld/client.go
+++ helloworld/client.go
@@ -17,12 +17,15 @@ package helloworld
 
 import (
 	"context"
+	"fmt"
 
 	trillian "github.com/google/trillian"
+	"github.com/google/trillian-examples/helloworld/personality"
 	"github.com/google/trillian/merkle/logverifier"
 	"github.com/google/trillian/merkle/rfc6962/hasher"
 
 	"github.com/google/trillian-examples/formats/log"
+	"golang.org/x/mod/sumdb/note"
 )
 
 // Personality is the front-end for the Trillian log.
@@ -30,41 +33,36 @@ type Personality interface {
 	// Append stores an entry in the log and, once it is there, returns a
 	// new checkpoint that commits to the new entry (in addition to all
 	// previous ones).
-	Append(context.Context, []byte) (*log.Checkpoint, error)
+	Append(ctx context.Context, entry []byte) (personality.SignedCheckpoint, error)
 
-	// ProveIncl proves the inclusion of a given entry.  It is the job of
-	// the client to verify this inclusion proof in order to convince
+	// ProveIncl proves the inclusion of a given entry in a given checkpoint size.
+	// It is the job of the client to verify this inclusion proof in order to convince
 	// itself that the entry really has been included in the log.
-	ProveIncl(context.Context, *log.Checkpoint, []byte) (*trillian.Proof, error)
+	ProveIncl(ctx context.Context, cpSize uint64, entry []byte) (*trillian.Proof, error)
 
-	// UpdateChkpt provides a new checkpoint and proves it is consistent
-	// with an old one.  Again, it is the job of the client to verify this
-	// consistency proof.
-	UpdateChkpt(context.Context, *log.Checkpoint) (*log.Checkpoint, *trillian.Proof, error)
+	// UpdateChkpt provides a new checkpoint along with a consistency proof to it
+	// from the specified older checkpoint size.
+	// Again, it is the job of the client to verify this consistency proof.
+	UpdateChkpt(ctx context.Context, oldCPSize uint64) (personality.SignedCheckpoint, *trillian.Proof, error)
 }
 
 // A client is a verifier that maintains a checkpoint as state.
 type Client struct {
-	v      logverifier.LogVerifier
-	chkpt  *log.Checkpoint
-	person Personality
+	v           logverifier.LogVerifier
+	chkpt       *log.Checkpoint
+	person      Personality
+	sigVerifier note.Verifier
 }
 
 // NewClient creates a new client with an empty checkpoint and a given
 // personality to talk to.  In real usage, a client should persist this
 // checkpoint across different runs to ensure consistency.
-func NewClient(prsn Personality) Client {
+func NewClient(prsn Personality, nv note.Verifier) Client {
 	v := logverifier.New(hasher.DefaultHasher)
-	var rootHash []byte
-	chkpt := &log.Checkpoint{
-		Ecosystem: "Hello world v0",
-		Size:      0,
-		Hash:      rootHash,
-	}
 	return Client{
-		person: prsn,
-		v:      v,
-		chkpt:  chkpt,
+		person:      prsn,
+		v:           v,
+		sigVerifier: nv,
 	}
 }
 
@@ -81,17 +79,26 @@ func (c Client) VerIncl(entry []byte, pf *trillian.Proof) bool {
 // UpdateChkpt allows a client to update its stored checkpoint.  In a real use
 // case it would be important for the client to check the signature contained
 // in the checkpoint before verifying consistency.
-func (c Client) UpdateChkpt(chkptNew *log.Checkpoint, pf *trillian.Proof) bool {
+func (c Client) UpdateChkpt(chkptNewRaw personality.SignedCheckpoint, pf *trillian.Proof) error {
+	n, err := note.Open(chkptNewRaw, note.VerifierList(c.sigVerifier))
+	if err != nil {
+		return fmt.Errorf("failed to verify checkpoint: %w", err)
+	}
+	chkptNew := &log.Checkpoint{}
+	_, err = chkptNew.Unmarshal([]byte(n.Text))
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal new checkpoint: %w", err)
+	}
 	// If there is no checkpoint then just use this one no matter what.
 	if c.chkpt.Size != 0 {
 		// Else make sure this new checkpoint is consistent with the current one.
 		hashes := pf.GetHashes()
 		if err := c.v.VerifyConsistencyProof(int64(c.chkpt.Size), int64(chkptNew.Size),
 			c.chkpt.Hash, chkptNew.Hash, hashes); err != nil {
-			return false
+			return fmt.Errorf("failed to verify consistency proof: %w", err)
 		}
 	}
 	// If all is good then set this as the new checkpoint.
 	c.chkpt = chkptNew
-	return true
+	return nil
 }

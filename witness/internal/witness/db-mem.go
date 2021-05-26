279fa8114a58e786b58aae9888d42f66e4ec02ae
Sarah Meiklejohn <meiklejohn@google.com>
2021-05-26 11:40:23 +0100

    adding signature verification

--- witness/internal/witness/db-mem.go
+++ witness/internal/witness/db-mem.go
@@ -11,24 +11,24 @@ import (
 type Database struct {
 	// For now this is going to be an in-memory map from logIds to lists of
 	// checkpoints.
-	logMap map[string][]log.Checkpoint
+	logMap map[string][]*log.Checkpoint
 }
 
 // GetLatest reads the latest checkpoint written to the DB for a given logId.
 func (d *Database) GetLatest(logId string) (*log.Checkpoint, error) {
 	if val, ok := d.logMap[logId]; ok {
-		return &val[len(val)-1], nil
+		return val[len(val)-1], nil
 	} else {
 		return nil, errors.New(fmt.Sprintf("Witness does not support log %s", logId))
 	}
 }
 
 // SetCheckpoint writes the checkpoint to the DB for a given logId.
-func (d *Database) SetCheckpoint(logId string, c log.Checkpoint) error {
+func (d *Database) SetCheckpoint(logId string, c *log.Checkpoint) error {
 	if val, ok := d.logMap[logId]; ok {
 		val = append(val, c)
 	} else {
-		d.logMap[logId] = []log.Checkpoint{c}
+		d.logMap[logId] = []*log.Checkpoint{c}
 	}
 	return nil
 }

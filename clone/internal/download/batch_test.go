91281ca246cc658189c6948c0a28970862770bc6
Martin Hutchinson <mhutchinson@gmail.com>
2021-06-11 14:07:28 +0100

    Tool that bulk downloads CT logs, respecting backoffs (#338)
    The tool clones leaves from the log as quickly as possible to a MySQL instance provisioned by the user. This is the foundation for doing local processing of all data, such as building a verifiable map.
    
    This is optimized very heavily towards downloading a large amount of data quickly, while respecting the log if it asks the client to back off. Missing features:
     * downloading/persisting checkpoints & verifying the leaf data
     * terminating cleanly when the end of the log is reached
    
    The design ensures that leaves are never written without any gaps in order to simplify the local state required.

---
+++ clone/internal/download/batch_test.go
@@ -0,0 +1,110 @@
+// Copyright 2021 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package download
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFetchWorkerRun(t *testing.T) {
+	rangec := make(chan leafRange, 10)
+	errc := make(chan error)
+	var first uint64
+	var batchSize uint = 10
+
+	fakeFetch := func(start uint64, leaves [][]byte) error {
+		return nil
+	}
+	fw := fetchWorker{
+		label:      "solipsism",
+		start:      first,
+		increment:  uint64(batchSize),
+		count:      batchSize,
+		out:        rangec,
+		errc:       errc,
+		batchFetch: fakeFetch,
+	}
+
+	go fw.run()
+
+	for i := 0; i < 10; i++ {
+		select {
+		case err := <-errc:
+			t.Fatal(err)
+		case r := <-rangec:
+			if got, want := r.start, uint64(i*10); got != want {
+				t.Errorf("%d got != want (%d != %d)", i, got, want)
+			}
+		}
+	}
+}
+
+func TestBulk(t *testing.T) {
+	leafc := make(chan []byte, 10)
+	errc := make(chan error)
+	var first uint64
+	var workers uint = 4
+	var batchSize uint = 10
+
+	fakeFetch := func(start uint64, leaves [][]byte) error {
+		for i := range leaves {
+			leaves[i] = []byte(fmt.Sprintf("%d.%d", start, i))
+		}
+		return nil
+	}
+	go Bulk(first, fakeFetch, workers, batchSize, leafc, errc)
+
+	for i := 0; i < 1000; i++ {
+		select {
+		case err := <-errc:
+			t.Fatal(err)
+		case l := <-leafc:
+			tens := (i / 10) * 10
+			units := i % 10
+			if got, want := string(l), fmt.Sprintf("%d.%d", tens, units); got != want {
+				t.Errorf("%d got != want (%q != %q)", i, got, want)
+			}
+		}
+	}
+}
+
+func BenchmarkBulk(b *testing.B) {
+	leafc := make(chan []byte, 10)
+	errc := make(chan error)
+	var first uint64
+	var workers uint = 20
+	var batchSize uint = 10
+
+	fakeFetch := func(start uint64, leaves [][]byte) error {
+		for i := range leaves {
+			// Allocate a non-trivial amount of memory for the leaf.
+			leaf := make([]byte, 1024)
+			leaves[i] = leaf
+		}
+		return nil
+	}
+	go Bulk(first, fakeFetch, workers, batchSize, leafc, errc)
+
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < 1000; i++ {
+			select {
+			case err := <-errc:
+				b.Fatal(err)
+			case <-leafc:
+			}
+		}
+	}
+}
---
+++ clone/internal/download/http.go
@@ -0,0 +1,58 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package download
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+// NewHTTPFetcher returns an HTTPFetcher that gets paths appended to the given prefix.
+func NewHTTPFetcher(prefix *url.URL) HTTPFetcher {
+	return HTTPFetcher{
+		baseURL: prefix,
+	}
+}
+
+// HTTPFetcher gets the data over HTTP(S).
+// This is thread safe.
+type HTTPFetcher struct {
+	baseURL *url.URL
+}
+
+// GetData gets the data at the given path.
+func (f HTTPFetcher) GetData(path string) ([]byte, error) {
+	u, err := f.baseURL.Parse(path)
+	if err != nil {
+		return nil, fmt.Errorf("url (%s) failed to parse(%s): %w", f.baseURL, path, err)
+	}
+	target := u.String()
+	resp, err := http.Get(target)
+	if err != nil {
+		return nil, fmt.Errorf("http.Get: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		// Could be worth returning a special error when we've been explicitly told to back off.
+		return nil, fmt.Errorf("GET %v: %v", target, resp.Status)
+	}
+	// TODO(mhutchinson): Consider using io.LimitReader and making it configurable.
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("ioutil.ReadAll: %w", err)
+	}
+	return data, nil
+}

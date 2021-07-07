2ccd76650bc541357a3f5fc024b0a08abb9c41d1
Andrej Rosano <andrej.rosano@f-secure.com>
2021-07-07 14:17:44 +0200

    Set ecosystem in log initialisation (#386)

--- serverless/deploy/github/sequence_and_integrate/entrypoint.sh
+++ serverless/deploy/github/sequence_and_integrate/entrypoint.sh
@@ -4,16 +4,27 @@ set -e
 
 function main {
     if [ "${SERVERLESS_LOG_PRIVATE_KEY}" == "" ]; then
-    echo "Missing log private key."
-    exit 1
+        echo "Missing log private key."
+        exit 1
     fi
     if [ "${INPUT_LOG_DIR}" == "" ]; then
-    echo "Missing log dir input."
-    exit 1
+        echo "Missing log dir input."
+        exit 1
     fi
     echo "::debug:Log directory is ${GITHUB_WORKSPACE}/${INPUT_LOG_DIR}"
 
     cd ${GITHUB_WORKSPACE}
+
+    if [ ! -f "${INPUT_LOG_DIR}/checkpoint" ]; then
+        echo "::debug:No checkpoint file - initialising log"
+        if [ -z "${INPUT_ECOSYSTEM}" ]; then 
+            /bin/integrate --storage_dir="${INPUT_LOG_DIR}" --initialise --logtostderr
+        else
+            /bin/integrate --storage_dir="${INPUT_LOG_DIR}" --ecosystem="${INPUT_ECOSYSTEM}" --initialise --logtostderr
+        fi
+
+        exit
+    fi
 
     PENDING="${INPUT_LOG_DIR}/leaves/pending"
     if [ ! -d "${PENDING}" ]; then
@@ -21,17 +32,16 @@ function main {
         exit
     fi
 
-    if [ ! -f "${INPUT_LOG_DIR}/checkpoint" ]; then
-        echo "::debug:No checkpoint file - initialising log"
-        /bin/integrate --storage_dir="${INPUT_LOG_DIR}" --initialise --logtostderr
-    fi
-
     echo "::debug:Sequencing..."
     /bin/sequence --storage_dir="${INPUT_LOG_DIR}" --logtostderr --entries "${PENDING}/*"
     rm ${PENDING}/*
 
     echo "::debug:Integrating..."
-    /bin/integrate --storage_dir="${INPUT_LOG_DIR}" ${SEQUENCE_FLAGS} --logtostderr
+    if [ -z "${INPUT_ECOSYSTEM}" ]; then 
+        /bin/integrate --storage_dir="${INPUT_LOG_DIR}" --logtostderr
+    else
+        /bin/integrate --storage_dir="${INPUT_LOG_DIR}" --ecosystem="${INPUT_ECOSYSTEM}" --logtostderr
+    fi
 }
 
 main

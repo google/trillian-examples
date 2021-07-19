f11697bdf778f0c96091683a7fdb15170cbe382a
Paul Hadfield <hadfieldp@google.com>
2021-07-19 16:53:38 +0100

    Add main loop and skeleton of createCheckpointPR, spawnFeeder

--- serverless/cmd/feeder/github/main.go
+++ serverless/cmd/feeder/github/main.go
@@ -20,6 +20,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"path"
 	"time"
 
 	"github.com/golang/glog"
@@ -57,14 +59,23 @@ func main() {
 	opts := mustValidateFlagsAndEnv()
 	ctx := context.Background()
 
-	witnessRepo, cleanupFn, err := setupWitnessRepo(ctx, opts)
+	// Make a tempdir to clone the witness (forked) log into.
+	tmpDir, err := os.MkdirTemp(os.TempDir(), "feeder-github")
 	if err != nil {
-		if cleanupFn != nil {
-			cleanupFn()
+		glog.Exitf("Error creating temp dir: %v", err)
+	}
+	opts.witnessClonePath = tmpDir
+
+	defer func() {
+		if err := os.RemoveAll(tmpDir); err != nil {
+			glog.Warning("RemoveAll err: %v", err)
 		}
+	}()
+
+	witnessRepo, err := setupWitnessRepo(ctx, opts)
+	if err != nil {
 		glog.Exitf("Error during set-up: %v", err)
 	}
-	defer cleanupFn()
 
 	//   3. auth to Github
 	//
@@ -78,8 +89,18 @@ func main() {
 	//   5. create checkpoint PR
 	//   6. clean up
 	//   7. --end of cycle--
+	fmt.Println("[Starting feeding]---------------------------------------------------")
+	for {
+		if err = createCheckpointPR(ctx, opts, witnessRepo); err != nil || opts.feederInterval == 0 {
+			break
+		}
+		<-time.After(opts.feederInterval)
+	}
+	if err != nil {
+		glog.Exitf("CreateCheckpointPR error: %v", err)
+	}
 
-	CreateCheckpointPR(witnessRepo)
+	fmt.Println("[Completed feeding]--------------------------------------------------")
 }
 
 type options struct {
@@ -88,6 +109,7 @@ type options struct {
 
 	logOwnerRepo, logRepoPath string
 	witnessOwnerRepo          string
+	witnessClonePath          string
 
 	feederConfigFile string
 	feederInterval   time.Duration
@@ -122,8 +144,8 @@ func mustValidateFlagsAndEnv() *options {
 
 	return &options{
 		logOwnerRepo:     *logOwnerRepo,
-		witnessOwnerRepo: *witnessOwnerRepo,
 		logRepoPath:      *logRepoPath,
+		witnessOwnerRepo: *witnessOwnerRepo,
 		feederConfigFile: *feederConfig,
 		feederInterval:   *interval,
 		githubAuthToken:  githubAuthToken,
@@ -132,29 +154,17 @@ func mustValidateFlagsAndEnv() *options {
 	}
 }
 
-func setupWitnessRepo(ctx context.Context, opts *options) (*git.Repository, func(), error) {
-	// Make a tempdir to clone the witness (forked) log into.
-	forkedLogDir, err := os.MkdirTemp(os.TempDir(), "feeder-github")
-	if err != nil {
-		return nil, nil, fmt.Errorf("Error creating temp dir: %v", err)
-	}
-
-	cleanup := func() {
-		if err := os.RemoveAll(forkedLogDir); err != nil {
-			glog.Warning("RemoveAll err: %v", err)
-		}
-	}
-
+func setupWitnessRepo(ctx context.Context, opts *options) (*git.Repository, error) {
 	// Clone the fork of the log, so we can go on to make a PR against it.
 	//   git clone -o origin "https://${GIT_USERNAME}:${FEEDER_GITHUB_TOKEN}@github.com/${fork_repo}.git" "${temp}"
 	forkLogURL := fmt.Sprintf("https://%v:%v@github.com/%v.git", opts.gitUsername, opts.githubAuthToken, opts.witnessOwnerRepo)
-	forkRepo, err := git.PlainClone(forkedLogDir, false, &git.CloneOptions{
+	forkRepo, err := git.PlainClone(opts.witnessClonePath, false, &git.CloneOptions{
 		URL: forkLogURL,
 	})
 	if err != nil {
-		return nil, cleanup, fmt.Errorf("Failed to clone fork repo %q: %v", forkLogURL, err)
+		return nil, fmt.Errorf("Failed to clone fork repo %q: %v", forkLogURL, err)
 	}
-	glog.Infof("Cloned %q into %q", forkLogURL, forkedLogDir)
+	glog.Infof("Cloned %q into %q", forkLogURL, opts.witnessClonePath)
 
 	// Create a remote -> logOwnerRepo
 	//  git remote add upstream "https://github.com/${log_repo}.git"
@@ -164,7 +174,7 @@ func setupWitnessRepo(ctx context.Context, opts *options) (*git.Repository, func
 		URLs: []string{logURL},
 	})
 	if err != nil {
-		return nil, cleanup, fmt.Errorf("Failed to add remote %q to fork repo: %v", logURL, err)
+		return nil, fmt.Errorf("Failed to add remote %q to fork repo: %v", logURL, err)
 	}
 	glog.Infof("Added remote upstream->%q for %q: %v", logURL, opts.witnessOwnerRepo, logRemote)
 
@@ -173,17 +183,17 @@ func setupWitnessRepo(ctx context.Context, opts *options) (*git.Repository, func
 	//  git config user.email "${GIT_EMAIL}"
 	cfg, err := forkRepo.Config()
 	if err != nil {
-		return nil, cleanup, fmt.Errorf("Failed to read config for repo %q: %v", opts.witnessOwnerRepo, err)
+		return nil, fmt.Errorf("Failed to read config for repo %q: %v", opts.witnessOwnerRepo, err)
 	}
 	cfg.User.Name = opts.gitUsername
 	cfg.User.Email = opts.gitEmail
 	if err = forkRepo.SetConfig(cfg); err != nil {
-		return nil, cleanup, fmt.Errorf("Failed to update config for repo %q: %v", opts.witnessOwnerRepo, err)
+		return nil, fmt.Errorf("Failed to update config for repo %q: %v", opts.witnessOwnerRepo, err)
 	}
 
 	//  git fetch --all
 	if err = forkRepo.FetchContext(ctx, &git.FetchOptions{}); err != nil && err != git.NoErrAlreadyUpToDate {
-		return nil, cleanup, fmt.Errorf("Failed to fetch repo %q: %v", *witnessOwnerRepo, err)
+		return nil, fmt.Errorf("Failed to fetch repo %q: %v", *witnessOwnerRepo, err)
 	}
 
 	//  git branch -u upstream/master
@@ -211,13 +221,44 @@ func setupWitnessRepo(ctx context.Context, opts *options) (*git.Repository, func
 	   	remote = upstream
 	   	merge = refs/heads/master
 	*/
-	return forkRepo, cleanup, nil
+	return forkRepo, nil
 }
 
-func CreateCheckpointPR(*git.Repository) {
-	fmt.Println("creating checkpoint PR ...")
+func createCheckpointPR(ctx context.Context, opts *options, forkRepo *git.Repository) error {
 
-	// 1.
+	// 1. git pull
 
-	//
+	// 2. kick off feeder. (TODO: refactor feeder to a library so we don't need to spawn a process)
+	witnessed, err := spawnFeeder(ctx, opts)
+	if err != nil {
+		return err
+	}
+
+	// 3. did feeder add a signuture or update the existing checkpoint.witnessed file?
+	if !witnessed {
+		fmt.Println("No signatures added")
+		return nil
+	}
+
+	// TODO...
+
+	fmt.Println("[Feed cycle complete]------------------------------------------------")
+	return nil
+}
+
+func spawnFeeder(ctx context.Context, opts *options) (bool, error) {
+	// TODO: kick off feeder process
+	// -config_file="${feeder_conf}" --logtostderr --input="https://raw.githubusercontent.com/${log_repo}/master/${log_path}/checkpoint" --output="${temp}/${log_path}/checkpoint.witnessed" -v 2
+	cmd := exec.Command("feeder", fmt.Sprintf("--config_file=%v", opts.feederConfigFile),
+		fmt.Sprintf("--input=\"https://raw.githubusercontent.com/%v/master/%v/checkpoint\"", opts.logOwnerRepo, opts.logRepoPath),
+		fmt.Sprintf("--output=\"%v/checkpoint.witnessed", path.Join(opts.witnessClonePath, opts.logRepoPath)),
+		"-v 2")
+
+	out, err := cmd.Output()
+	if err != nil {
+		return false, err
+	}
+
+	fmt.Sprintf("feeder output:\n%v\n", out)
+	return true, nil
 }

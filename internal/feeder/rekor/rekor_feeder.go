315d00da9349bc6c545e02edf8304b21e632c201
Al Cutter <al@google.com>
2021-12-06 12:36:47 +0000

    [Witness] Extract feeder implementations from their main files (#570)
    Extract impls of:
      * pixel feeder
      * serverless feeder
      * sumDB feeder
      * Rekor feeder
    
    This will make it possible to use alternative `main` files.

---
+++ internal/feeder/rekor/rekor_feeder.go
@@ -0,0 +1,138 @@
+// Copyright 2021 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package rekor is an implementation of a witness feeder for the Sigstore log: Rekór.
+package rekor
+
+import (
+	"context"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/google/trillian-examples/formats/log"
+	"github.com/google/trillian-examples/internal/feeder"
+	"github.com/google/trillian-examples/serverless/config"
+
+	i_note "github.com/google/trillian-examples/internal/note"
+	wit_http "github.com/google/trillian-examples/witness/golang/client/http"
+)
+
+// logInfo is a partial representation of the JSON object returned by Rekór's
+// api/v1/log request.
+type logInfo struct {
+	// SignedTreeHead contains a Rekór checkpoint.
+	SignedTreeHead string `json:"signedTreeHead"`
+}
+
+// proof is a partial representation of the JSON struct returned by the Rekór
+// api/v1/log/proof request.
+type proof struct {
+	Hashes []string `json:"hashes"`
+}
+
+// FeedLog feeds checkpoints from the source log to the witness.
+// If interval is non-zero, this function will return when the context is done, otherwise it will perform
+// one feed cycle and return.
+func FeedLog(ctx context.Context, l config.Log, w wit_http.Witness, timeout time.Duration, interval time.Duration) error {
+	lURL, err := url.Parse(l.URL)
+	if err != nil {
+		return fmt.Errorf("invalid LogURL %q: %v", l.URL, err)
+	}
+	logSigV, err := i_note.NewVerifier(l.PublicKeyType, l.PublicKey)
+	if err != nil {
+		return err
+	}
+
+	fetchCP := func(ctx context.Context) ([]byte, error) {
+		li := logInfo{}
+		if err := getJSON(ctx, lURL, "api/v1/log", &li); err != nil {
+			return nil, fmt.Errorf("failed to fetch log info: %v", err)
+		}
+		return []byte(li.SignedTreeHead), nil
+	}
+	fetchProof := func(ctx context.Context, from, to log.Checkpoint) ([][]byte, error) {
+		if from.Size == 0 {
+			return [][]byte{}, nil
+		}
+		cp := proof{}
+		if err := getJSON(ctx, lURL, fmt.Sprintf("api/v1/log/proof?firstSize=%d&lastSize=%d", from.Size, to.Size), &cp); err != nil {
+			return nil, fmt.Errorf("failed to fetch log info: %v", err)
+		}
+		var err error
+		p := make([][]byte, len(cp.Hashes))
+		for i := range cp.Hashes {
+			p[i], err = hex.DecodeString(cp.Hashes[i])
+			if err != nil {
+				return nil, fmt.Errorf("invalid proof element at %d: %v", i, err)
+			}
+		}
+		return p, nil
+	}
+
+	opts := feeder.FeedOpts{
+		LogID:           l.ID,
+		LogOrigin:       l.Origin,
+		FetchCheckpoint: fetchCP,
+		FetchProof:      fetchProof,
+		LogSigVerifier:  logSigV,
+		Witness:         w,
+	}
+	if interval > 0 {
+		return feeder.Run(ctx, interval, opts)
+	}
+	_, err = feeder.FeedOnce(ctx, opts)
+	return err
+}
+
+func getJSON(ctx context.Context, base *url.URL, path string, s interface{}) error {
+	u, err := base.Parse(path)
+	if err != nil {
+		return fmt.Errorf("failed to parse URL: %v", err)
+	}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	req = req.WithContext(ctx)
+
+	rsp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to make request to %q: %v", u.String(), err)
+	}
+	defer rsp.Body.Close()
+
+	if rsp.StatusCode == 404 {
+		return os.ErrNotExist
+	}
+	if rsp.StatusCode != 200 {
+		return fmt.Errorf("unexpected status fetching %q: %s", u.String(), rsp.Status)
+	}
+
+	raw, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read body from %q: %v", u.String(), err)
+	}
+	if err := json.Unmarshal(raw, s); err != nil {
+		glog.Infof("Got body:\n%s", string(raw))
+		return fmt.Errorf("failed to unmarshal JSON: %v", err)
+	}
+	return nil
+}
--- serverless/cmd/feeder/main.go
+++ serverless/cmd/feeder/main.go
@@ -23,18 +23,14 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"net/url"
 	"sync"
 	"time"
 
 	"github.com/golang/glog"
 	"github.com/google/trillian-examples/formats/log"
-	"github.com/google/trillian-examples/internal/feeder"
-	"github.com/google/trillian-examples/serverless/client"
+	"github.com/google/trillian-examples/internal/feeder/serverless"
 	"github.com/google/trillian-examples/serverless/config"
-	"github.com/transparency-dev/merkle/rfc6962"
-	"golang.org/x/mod/sumdb/note"
 
 	wit_http "github.com/google/trillian-examples/witness/golang/client/http"
 	yaml "gopkg.in/yaml.v2"
@@ -71,67 +67,18 @@ func main() {
 		URL: u,
 	}
 
+	ctx := context.Background()
 	wg := &sync.WaitGroup{}
 	for _, l := range cfg.Logs {
 		wg.Add(1)
 		go func(l config.Log, w wit_http.Witness) {
 			defer wg.Done()
-			if err := feedLog(l, witness, *timeout, *interval); err != nil {
+			if err := serverless.FeedLog(ctx, l, witness, *timeout, *interval); err != nil {
 				glog.Errorf("feedLog: %v", err)
 			}
 		}(l, witness)
 	}
 	wg.Wait()
-}
-
-func feedLog(l config.Log, w wit_http.Witness, timeout time.Duration, interval time.Duration) error {
-	lURL, err := url.Parse(l.URL)
-	if err != nil {
-		return fmt.Errorf("invalid LogURL %q: %v", l.URL, err)
-	}
-	f := newFetcher(lURL)
-	h := rfc6962.DefaultHasher
-
-	fetchCP := func(ctx context.Context) ([]byte, error) {
-		return f(ctx, "checkpoint")
-	}
-	fetchProof := func(ctx context.Context, from, to log.Checkpoint) ([][]byte, error) {
-		if from.Size == 0 {
-			return [][]byte{}, nil
-		}
-		pb, err := client.NewProofBuilder(ctx, to, h.HashChildren, f)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create proof builder for %q: %v", l.Origin, err)
-		}
-
-		conP, err := pb.ConsistencyProof(ctx, from.Size, to.Size)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create proof for %q(%d -> %d): %v", l.Origin, from.Size, to.Size, err)
-		}
-		return conP, nil
-	}
-
-	opts := feeder.FeedOpts{
-		LogID:           l.ID,
-		LogOrigin:       l.Origin,
-		FetchCheckpoint: fetchCP,
-		FetchProof:      fetchProof,
-		LogSigVerifier:  mustCreateVerifier(l.PublicKey),
-		Witness:         w,
-	}
-	if interval > 0 {
-		return feeder.Run(context.Background(), interval, opts)
-	}
-	_, err = feeder.FeedOnce(context.Background(), opts)
-	return err
-}
-
-func mustCreateVerifier(pub string) note.Verifier {
-	v, err := note.NewVerifier(pub)
-	if err != nil {
-		glog.Exitf("Failed to create signature verifier from %q: %v", pub, err)
-	}
-	return v
 }
 
 func readConfig(f string) (*Config, error) {
@@ -149,43 +96,4 @@ func readConfig(f string) (*Config, error) {
 		}
 	}
 	return &cfg, nil
-}
-
-// TODO(al): factor this stuff out and share between tools:
-
-// newFetcher creates a Fetcher for the log at the given root location.
-func newFetcher(root *url.URL) client.Fetcher {
-	get := getByScheme[root.Scheme]
-	if get == nil {
-		panic(fmt.Errorf("unsupported URL scheme %s", root.Scheme))
-	}
-
-	return func(ctx context.Context, p string) ([]byte, error) {
-		u, err := root.Parse(p)
-		if err != nil {
-			return nil, err
-		}
-		return get(ctx, u)
-	}
-}
-
-var getByScheme = map[string]func(context.Context, *url.URL) ([]byte, error){
-	"http":  readHTTP,
-	"https": readHTTP,
-	"file": func(_ context.Context, u *url.URL) ([]byte, error) {
-		return ioutil.ReadFile(u.Path)
-	},
-}
-
-func readHTTP(ctx context.Context, u *url.URL) ([]byte, error) {
-	req, err := http.NewRequest("GET", u.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := http.DefaultClient.Do(req.WithContext(ctx))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
 }

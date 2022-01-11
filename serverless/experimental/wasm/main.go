08f1247f530c33af0494fac65fd9f54c9aeb53c5
Jay Hou <jayhou@google.com>
2022-01-11 11:41:30 -0800

    Add ctx for wasm WriteCheckpoint call.

--- serverless/experimental/wasm/main.go
+++ serverless/experimental/wasm/main.go
@@ -126,7 +126,8 @@ func integrate() js.Func {
 			}
 		}
 
-		newCp, err := log.Integrate(context.Background(), *cp, logStorage, rfc6962.DefaultHasher)
+		ctx := context.Background()
+		newCp, err := log.Integrate(ctx, *cp, logStorage, rfc6962.DefaultHasher)
 		if err != nil {
 			logMsg(fmt.Sprintf("Failed to integrate: %q", err))
 			panic(err)
@@ -146,7 +147,7 @@ func integrate() js.Func {
 			logMsg(err.Error())
 			return nil
 		}
-		if err := logStorage.WriteCheckpoint(nRaw); err != nil {
+		if err := logStorage.WriteCheckpoint(ctx, nRaw); err != nil {
 			logMsg(err.Error())
 			return nil
 		}

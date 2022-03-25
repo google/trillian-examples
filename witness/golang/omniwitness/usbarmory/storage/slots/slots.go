af8f706176327aa42820d191369ec0d453285a28
Al Cutter <al@google.com>
2022-03-25 13:51:44 +0000

    Add support for slots & partition

---
+++ witness/golang/omniwitness/usbarmory/storage/slots/slots.go
@@ -0,0 +1,133 @@
+// Copyright 2022 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slots
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
+
+// Geometry describes the physical layout of a Partition and its slots on the
+// underlying storage.
+type Geometry struct {
+	// Start identifies the address of first block which is part of a partition.
+	Start uint
+	// Length is the number of blocks covered by this partition.
+	// i.e. [Start, Start+Length) is the range of blocks covered by this partition.
+	Length uint
+	// SlotLengths is an ordered list containing the lengths of the slot(s)
+	// allocated within this partition.
+	// For obvious reasons, great care must be taken if, once data has been written
+	// to one or more slots, the values specified in this list at the time the data
+	// was written are changed.
+	SlotLengths []uint
+}
+
+// Validate checks that the geometry is self-consistent.
+func (g Geometry) Validate() error {
+	t := uint(0)
+	for _, l := range g.SlotLengths {
+		t += l
+	}
+	if t > g.Length {
+		return fmt.Errorf("invalid geometry: total slot length (%d blocks) exceeds overall length (%d blocks)", t, g.Length)
+	}
+	return nil
+}
+
+// OpenPartition returns a partition struct for accessing the slots described by the given
+// geometry using the provided read/write methods.
+func OpenPartition(rw BlockReaderWriter, geo Geometry) (*Partition, error) {
+	if err := geo.Validate(); err != nil {
+		return nil, err
+	}
+
+	ret := &Partition{
+		dev: rw,
+	}
+
+	b := geo.Start
+	for i, l := range geo.SlotLengths {
+		j, err := OpenJournal(ret.dev, b, l)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open journal for slot %d: %v", i, err)
+		}
+		ret.slots = append(ret.slots, Slot{
+			journal: j,
+		})
+		b += l
+	}
+
+	return ret, nil
+}
+
+// Partition describes the extent and layout of a single contiguous region of
+// underlying block storage.
+type Partition struct {
+	// dev provides the device-specific read/write functionality.
+	dev BlockReaderWriter
+	// slots decribes the layout of the slot(s) stored within this partition.
+	slots []Slot
+}
+
+// Open opens the specified slot, returns an error if the slot is out of bounds.
+// Upon successful return, the specified slot remains locked until Close is called on it.
+func (p *Partition) Open(slot uint) (*Slot, error) {
+	if l := uint(len(p.slots)); slot >= l {
+		return nil, fmt.Errorf("invalid slot %d (partition has %d slots)", slot, l)
+	}
+	s := &p.slots[slot]
+	return s, nil
+}
+
+// Slot represents the current data in a slot.
+type Slot struct {
+	// mu guards access to this Slot.
+	mu sync.RWMutex
+
+	journal *Journal
+}
+
+// Read returns the last data successfully written to the slot, along with a token
+// which can be used with WriteIf.
+func (s *Slot) Read() ([]byte, uint32, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.journal.current.Data, s.journal.current.Revision, nil
+}
+
+// Write writes the provided data to the slot.
+// Upon successful completion, this data will be returned by future calls to Read
+// until another successful Write call is mode.
+// If the call to Write fails, future calls to Read will return the previous
+// successfully written data, if any.
+func (s *Slot) Write(p []byte) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.journal.Update(p)
+}
+
+// CheckAndWrite behaves like Write, with the exception that it will immediately
+// return an error if the slot has been successfully written to since the Read call
+// which produced the passed-in token.
+func (s *Slot) CheckAndWrite(token uint32, p []byte) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if s.journal.current.Revision != token {
+		return errors.New("invalid token, slot updated since then")
+	}
+	return s.journal.Update(p)
+}
---
+++ witness/golang/omniwitness/usbarmory/storage/slots/slots_test.go
@@ -0,0 +1,125 @@
+package slots
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/trillian-examples/witness/golang/omniwitness/usbarmory/storage/testonly"
+)
+
+func TestOpenPartition(t *testing.T) {
+	type slotGeo struct {
+		Start  uint
+		Length uint
+	}
+	toSlotGeo := func(in []Slot) []slotGeo {
+		r := make([]slotGeo, len(in))
+		for i := range in {
+			r[i] = slotGeo{
+				Start:  in[i].journal.start,
+				Length: in[i].journal.length,
+			}
+		}
+		return r
+	}
+
+	const devBlockSize = 32
+
+	for _, test := range []struct {
+		name      string
+		geo       Geometry
+		wantErr   bool
+		wantSlots []slotGeo
+	}{
+		{
+			name: "free space remaining",
+			geo: Geometry{
+				Start:       10,
+				Length:      10,
+				SlotLengths: []uint{1, 1, 2, 4},
+			},
+			wantSlots: []slotGeo{
+				{Start: 10, Length: 1},
+				{Start: 11, Length: 1},
+				{Start: 12, Length: 2},
+				{Start: 14, Length: 4},
+			},
+		}, {
+			name: "fully allocated",
+			geo: Geometry{
+				Start:       10,
+				Length:      10,
+				SlotLengths: []uint{1, 1, 2, 4, 2},
+			},
+			wantSlots: []slotGeo{
+				{Start: 10, Length: 1},
+				{Start: 11, Length: 1},
+				{Start: 12, Length: 2},
+				{Start: 14, Length: 4},
+				{Start: 18, Length: 2},
+			},
+		}, {
+			name: "over allocated",
+			geo: Geometry{
+				Start:       10,
+				Length:      10,
+				SlotLengths: []uint{1, 1, 2, 4, 3},
+			},
+			wantErr: true,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			dev := testonly.NewMemDev(t, devBlockSize)
+			p, err := OpenPartition(dev, test.geo)
+			if gotErr := err != nil; gotErr != test.wantErr {
+				t.Fatalf("Got %v, wantErr %t", err, test.wantErr)
+			}
+			if test.wantErr {
+				return
+			}
+			if diff := cmp.Diff(toSlotGeo(p.slots), test.wantSlots); diff != "" {
+				t.Fatalf("Got diff: %s", diff)
+			}
+		})
+	}
+}
+
+func memPartition(t *testing.T) (*Partition, testonly.MemDev) {
+	t.Helper()
+	md := testonly.NewMemDev(t, 32)
+	geo := Geometry{
+		Start:       10,
+		Length:      10,
+		SlotLengths: []uint{1, 1, 2, 4},
+	}
+	p, err := OpenPartition(md, geo)
+	if err != nil {
+		t.Fatalf("Failed to create mem partition: %v", err)
+	}
+	return p, md
+}
+
+func TestOpenSlot(t *testing.T) {
+	p, _ := memPartition(t)
+	for _, test := range []struct {
+		name    string
+		slot    uint
+		wantErr bool
+	}{
+		{
+			name: "works",
+			slot: 0,
+		}, {
+			name:    "invalid slot: too big",
+			slot:    uint(len(p.slots)),
+			wantErr: true,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := p.Open(test.slot)
+			if gotErr := err != nil; gotErr != test.wantErr {
+				t.Fatalf("Failed to open slot: %v", err)
+			}
+		})
+	}
+}

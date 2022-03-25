58e388176ffe2c441761a9274b4f73cb4a7aab20
Al Cutter <al@google.com>
2022-03-25 13:55:46 +0000

    Example use

--- witness/golang/omniwitness/usbarmory/main.go
+++ witness/golang/omniwitness/usbarmory/main.go
@@ -23,6 +23,7 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
 	"flag"
 	"time"
 
@@ -31,7 +32,10 @@ import (
 	"golang.org/x/mod/sumdb/note"
 	"golang.org/x/sync/errgroup"
 
-	_ "github.com/usbarmory/tamago/board/f-secure/usbarmory/mark-two"
+	"github.com/google/trillian-examples/witness/golang/omniwitness/usbarmory/storage"
+	"github.com/google/trillian-examples/witness/golang/omniwitness/usbarmory/storage/slots"
+
+	usbarmory "github.com/usbarmory/tamago/board/f-secure/usbarmory/mark-two"
 )
 
 const (
@@ -49,6 +53,58 @@ func main() {
 	// We parse the flags despite declaring none ourselves so libraries are
 	// happy (looking at you, glog).
 	flag.Parse()
+
+	if err := usbarmory.MMC.Detect(); err != nil {
+		glog.Exitf("Failed to detect MMC: %v", err)
+	}
+
+	// dev is our access to the MMC storage.
+	dev := &storage.Device{
+		Card: usbarmory.MMC,
+	}
+
+	// geo defines the location and layout of our storage partition.
+	geo := slots.Geometry{
+		// Start at block 10, for no particular reason
+		Start: 10,
+		// Length of ths partition is 100 blocks, because it just is.
+		Length: 100,
+		// SlotLengths defines the layout of slots within our partition.
+		// Slots are stored contiguously in the range of blocks [Start, Start+Length).
+		// We've configured 3 slots, each with 10 blocks to hold its journal.
+		// The remaining 70 blocks are unused.
+		SlotLengths: []uint{
+			10,
+			10,
+			10,
+		},
+	}
+	p, err := slots.OpenPartition(dev, geo)
+	if err != nil {
+		glog.Exitf("Failed to open partition: %v", err)
+	}
+
+	s, err := p.Open(0)
+	if err != nil {
+		glog.Exitf("Failed to open slot 0: %v", err)
+	}
+
+	data, token, err := s.Read()
+	if err != nil {
+		glog.Exitf("Failed to read slot 0 data: %v", err)
+	}
+
+	newData := make([]byte, 10)
+	if _, err := rand.Read(newData); err != nil {
+		glog.Exitf("Failed to get random data: %v", err)
+	}
+
+	data = append(data, newData...)
+	if err := s.CheckAndWrite(token, data); err != nil {
+		glog.Exitf("Failed to update data: %v", err)
+	}
+	glog.Infof("Data@%x:\n%x", token, data)
+
 	ctx := context.Background()
 	// This error group will be used to run all top level processes
 	g := errgroup.Group{}
